a0073d3beac94f805539114d62f3e6aa6c319ea7
MRMADDUCK <rileykanedavis@gmail.com>
2025-03-27 10:46:22 +0800

    Update cv-page-template.php

--- cv-page-template.php
+++ cv-page-template.php
@@ -3,7 +3,6 @@
 Template Name: CV
 */
 ?>
-<?php get_header(); /* Tells WordPress to include header.php */ ?>
 <?php require_once('page_header.php'); ?>
 <div class="container-fluid maincontainer">
     <div class="container">
