956c97d3fbed9a80226bad34727d3b3425de4990
MRMADDUCK <rileykanedavis@gmail.com>
2025-03-29 23:52:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="whitkins">WHTKINS</span></a>
+                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="whitkin">WHITKIN</span></a>
                 </div>
                 <div class="col-md-6">
                     <p>Terms and Conditions</p>
